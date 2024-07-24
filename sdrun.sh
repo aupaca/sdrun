e7b2528633f164a96f6e84656dd61cf317a353a2
artur <140577944+osartur@users.noreply.github.com>
2024-07-24 02:32:11 -0300

    Atualizar o sdrun.sh

--- sdrun.sh
+++ sdrun.sh
@@ -6,16 +6,16 @@ main() {
 	fi
 	src=$(pwd)/$1
 	dest=~/a.out
-	mv $src $dest
-	chmod +x $dest
+	mv "$src" "$dest"
+	chmod +x "$dest"
 	$dest
 	local status=$?
-	mv $dest $src
+	mv "$dest" "$src"
 	return $status
 }
 
 restore() {
-	mv $dest $src
+	mv "$dest" "$src"
 	echo
 	exit 1
 }
